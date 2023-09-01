de368c8c90756aa1e8ef8d2968e9214c1ee26960
R Givent A Simanjorang <79833996+a4smanjorg5@users.noreply.github.com>
2023-09-01 15:01:49 +0700

    add convoe page

---
+++ src/app/convoe/create/layout.js
@@ -0,0 +1,17 @@
+/**
+ * This file is part of the cretech repo.
+ *
+ * (c) a4smanjorg5
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export const metadata = {
+  title: 'Election Grid',
+  description: 'Make election letter',
+}
+
+export default function CreateLayout({ children }) {
+  return children
+}
